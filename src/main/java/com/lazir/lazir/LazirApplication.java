f3ebd648103cd08e0e0a2e885aa2d86ec715dfa6
HUNNY <fkrudgns@gmail.com>
2021-09-13 15:52:48 +0900

    first time for test git

---
+++ src/main/java/com/lazir/lazir/LazirApplication.java
@@ -0,0 +1,13 @@
+package com.lazir.lazir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazirApplication.class, args);
+	}
+
+}
